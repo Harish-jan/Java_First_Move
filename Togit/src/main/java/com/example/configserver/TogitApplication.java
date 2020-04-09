8aa2819160ac716e5ef3ed84854e907849e27e99
user <user@192.168.1.7>
2020-04-09 15:14:01 +0530

    done

---
+++ Togit/src/main/java/com/example/configserver/TogitApplication.java
@@ -0,0 +1,13 @@
+package com.example.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogitApplication.class, args);
+	}
+
+}
