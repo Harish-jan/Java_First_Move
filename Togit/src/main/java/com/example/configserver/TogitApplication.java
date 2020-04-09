2bc49cf84e78267e6ebc61206d81900d19338d10
user <user@192.168.1.8>
2020-04-09 23:31:47 +0530

    z

--- Togit/src/main/java/com/example/configserver/TogitApplication.java
+++ Togit/src/main/java/com/example/configserver/TogitApplication.java
@@ -3,7 +3,7 @@ package com.example.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//mama12345678966667789123
+//
 @SpringBootApplication
 public class TogitApplication {
 
